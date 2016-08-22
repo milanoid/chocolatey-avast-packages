bc1ae540c48189382e11f4dbd033945075153e65
Milan <milanvojnovic@gmail.com>
2016-08-22 12:04:58 +0200

    internet security editon

---
+++ avast-is-trial/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'avast-is-trial'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://files.avast.com/iavs9x/avast_internet_security_setup_online.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/silent"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'avast-is-trial*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
