0c7bdcb523efbbb0c84adb855319ab6b596e8e99
Milan Vojnovič <vojnovic@avast.com>
2016-09-02 21:46:39 +0200

    Removed unused Url64

--- avast-pro-trial/tools/chocolateyinstall.ps1
+++ avast-pro-trial/tools/chocolateyinstall.ps1
@@ -5,15 +5,13 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'avast-pro-trial'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://files.avast.com/iavs9x/avast_pro_antivirus_setup_online.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
-
+  
   silentArgs    = "/silent"
   validExitCodes= @(0, 3010, 1641)
 
